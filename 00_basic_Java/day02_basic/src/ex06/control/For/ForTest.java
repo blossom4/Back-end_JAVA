63e779b39c7fd7ca026ffdab25bf78cbf51ee258
blossom4 <cho8213456@gmail.com>
2021-07-30 10:19:29 +0900

    JAVA Repository

--- 00_basic_Java/day02_basic/src/ex06/control/For/ForTest.java
+++ 00_basic_Java/day02_basic/src/ex06/control/For/ForTest.java
@@ -12,7 +12,6 @@ public class ForTest {
 		sum = 0;
 		int count = 0;
 		for (int i = 1; i < 101; i++) {
-			
 			if (i % 3 == 0) {
 				sum += i;
 				count++;
---
+++ 99_Self_study/Chapter2/src/binary/Constant.java
@@ -0,0 +1,20 @@
+package binary;
+
+public class Constant {
+
+	public static void main(String[] args) {
+		final int MAX_NUM = 100;
+		final float PI = 3.14f;
+		
+		final int STUDENT_NUM = 30;
+		
+		
+		int num = 30;
+		if (num == STUDENT_NUM ) {
+			System.out.println("same");
+		}
+		
+		System.out.println(STUDENT_NUM);
+	}
+
+}
