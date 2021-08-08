141d04596e125f4f8630dcca3e49233193bc1a9a
blossom4 <cho8213456@gmail.com>
2021-08-08 18:12:05 +0900

    98 self study Chapter3

---
+++ 98_self_study/src/Chapter3/OperationEx2.java
@@ -0,0 +1,14 @@
+package Chapter3;
+
+public class OperationEx2 {
+
+	public static void main(String[] args) {
+		int num1 = 10, num2 = 10;
+		System.out.println(++num1);
+		System.out.println(num2++);
+		
+		System.out.println(3 > 5);
+		System.out.println(1 == 1);
+	}
+
+}
